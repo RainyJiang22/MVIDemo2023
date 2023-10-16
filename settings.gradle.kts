ef2857717977b37a8e9dedc7c5b63bfa5e3bba4f
jiangshiyu <3434481891@qq.com>
2023-10-16 11:22:50 +0800

    feature: Initial Project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MVIDemo2023"
+include(":app")
+ 
